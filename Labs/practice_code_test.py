d23a083290682cb6f79f144f2272eeccaa1f9761
cormac smith <cormacsmith@live.co.uk>
2020-12-10 19:41:58 +0000

    updated practice_code

--- Labs/practice_code_test.py
+++ Labs/practice_code_test.py
@@ -1,21 +1,31 @@
-import unittest
+# import unittest
 
-from practice_code import Person, Admin, Barber
+# # from practice_code import Person, Admin, Barber
+# from practice_code import A
 
-class Test(unittest.TestCase):
-    def setUp(self):
-        self.test = Barber('Joe', 12.50, 7)
-        self.values =[0, 'Joe']
+# class Test(unittest.TestCase):
+#     def setUp(self):
+#         self.test = A()
     
-    # check that a failure is returned
-    @unittest.expectedFailure
-    def test_calculate_salary(self):
-        self.assertEqual(self.test.calculate_salary(), self.values[1])
-    
-    # testing if salary is returned
-    def test_get_salary(self):
-        result = self.test.calculate_salary()
-        self.assertTrue(result, self.values[0])
+#     #testing if an error has been raised
+#     def test_veryprivate_setter(self):
+#         with self.assertRaises(TypeError):
+#             self.test.very_private = 0
 
-if __name__ == '__main__':
-    unittest.main()
+
+#     # def setUp(self):
+#     #     self.test = Barber('Joe', 12.50, 7)
+#     #     self.values =[0, 'Joe']
+    
+#     # # check that a failure is returned
+#     # @unittest.expectedFailure
+#     # def test_calculate_salary(self):
+#     #     self.assertEqual(self.test.calculate_salary(), self.values[1])
+    
+#     # # testing if salary is returned
+#     # def test_get_salary(self):
+#     #     result = self.test.calculate_salary()
+#     #     self.assertTrue(result, self.values[0])
+
+# if __name__ == '__main__':
+#     unittest.main()
